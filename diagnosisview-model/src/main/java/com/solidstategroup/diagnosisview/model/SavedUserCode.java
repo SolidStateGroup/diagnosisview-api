a42729dac5d8e356a633036897c4da17a0c06b89
Tom <tom@solidstategroup.com>
2018-07-31 17:56:28 +0100

    Dont return null values for favourites or history

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/SavedUserCode.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/SavedUserCode.java
@@ -1,5 +1,6 @@
 package com.solidstategroup.diagnosisview.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SavedUserCode {
 
     @ApiModelProperty(example = "coma")
