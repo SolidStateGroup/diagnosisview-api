3d532b47a70927977571d4cf6d02bc966f75c8ce
Tom <tom@solidstategroup.com>
2018-10-02 10:57:01 +0100

    Update reset code on reset password

--- diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/impl/UserServiceImpl.java
+++ diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/impl/UserServiceImpl.java
@@ -444,6 +444,8 @@ public class UserServiceImpl implements UserService {
 
         //Update the password and salt
         user.setPassword(resetDto.getNewPassword());
+        user.setResetExpiryDate(null);
+        user.setResetCode(null);
         return this.createOrUpdateUser(user, true);
     }
 
