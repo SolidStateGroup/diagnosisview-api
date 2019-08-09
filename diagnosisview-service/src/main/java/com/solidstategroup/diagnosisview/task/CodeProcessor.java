155aaa9cf2c31da39216261c663113e6c70fa65e
Pavlo Maks <pavlo@solidstategroup.com>
2019-08-09 18:10:20 +0100

    implemented code processor

---
+++ diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/task/CodeProcessor.java
@@ -0,0 +1,38 @@
+package com.solidstategroup.diagnosisview.task;
+
+import com.solidstategroup.diagnosisview.model.codes.Code;
+import com.solidstategroup.diagnosisview.service.CodeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Async Processor to process codes syncing.
+ *
+ * Created by Pavlo Maksymchuk.
+ */
+@Slf4j
+@Component
+public class CodeProcessor {
+
+    private final CodeService codeService;
+
+    @Autowired
+    public CodeProcessor(final CodeService codeService) {
+        this.codeService = codeService;
+    }
+
+    @Async
+    public void processBatch(List<Code> codes, int index) {
+        log.info("Starting code batch {} {}", index, codes.size());
+        long start = System.currentTimeMillis();
+
+        codeService.batchProcess(codes);
+
+        long stop = System.currentTimeMillis();
+        log.info("Finished code batch {}, timing {}", index, (stop - start));
+    }
+}
