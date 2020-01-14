d7155d0b640474932f2c851515a51c034f7d7a54
Pavlo Maks <pavlo@solidstategroup.com>
2020-01-14 15:50:20 +0000

    implemented BMJ sync for a code

--- diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/BmjBestPractices.java
+++ diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/BmjBestPractices.java
@@ -12,4 +12,14 @@ public interface BmjBestPractices {
      * code standard and then ICD-10 if SNOMED-CT does not return a result.
      */
     void syncBmjLinks();
+
+    /**
+     * Calls the BMJ Best Practice API to pull links urls
+     * for BMJ website for given Code string.
+     *
+     * Uses codes already stored in diagnosis view
+     * to make calls to the BMJ. Method will call first using SNOMED-CT
+     * code standard and then ICD-10 if SNOMED-CT does not return a result.
+     */
+    void syncBmjLinks(String code);
 }
