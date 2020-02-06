d61ef0bb26e2238b0834432b5f1a59548500c3c0
Pavlo Maksymchuk <pavlo.maksymchuk@yclip.co.uk>
2020-02-06 17:45:36 +0000

    implemented nhs choices service

---
+++ diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/NhsChoicesService.java
@@ -0,0 +1,28 @@
+package com.solidstategroup.diagnosisview.service;
+
+/**
+ * NHS Choices service, for retrieving data from NHS Choices.
+ *
+ * @author P Maksymchuk
+ * @created on 06/02/2020
+ */
+public interface NhsChoicesService {
+
+    /**
+     * Step 1 of update PV Codes from NHS Choices.
+     * Reads data from API and stores each condition as NhschoicesCondition.
+     * Will create new NhschoicesConditions and delete from PV if no longer found in API.
+     *
+     * //@throws ImportResourceException
+     */
+    void updateConditionsFromJob();
+
+    /**
+     * Step 2 of update PV Codes, synchronises NhschoicesConditions with Codes.
+     * If an NhschoicesCondition has been deleted, marks Code as externallyRemoved = true.
+     *
+     * //@throws ResourceNotFoundException
+     */
+    void syncConditionsWithCodes();//throws ResourceNotFoundException
+
+}
