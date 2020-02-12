07ead78d6b938185cd1eddae2b035d8c0e45a110
Pavlo Maks <pavlo@solidstategroup.com>
2020-02-12 16:39:43 +0000

    refactored Code standard types enums

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/enums/CodeStandardTypes.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/enums/CodeStandardTypes.java
@@ -1,20 +1,27 @@
 package com.solidstategroup.diagnosisview.model.codes.enums;
 
 /**
- * Used in testing etc where specific name of code standard type is used (e.g. PATIENTVIEW, EDTA). Stored in Lookups
+ * Used in testing etc where specific name of code standard type is used (e.g. NHS_CHOICES, EDTA). Stored in Lookups
  * in database.
- * Created by jamesr@solidstategroup.com
- * Created on 08/06/2016
  */
 public enum CodeStandardTypes {
     EDTA("EDTA"),
     ICD("ICD"),
-    PATIENTVIEW("PatientView"),
+    NHS_CHOICES("NHS Choices"),
     READ("READ"),
     SNOMED("SNOMED");
 
     private String name;
-    CodeStandardTypes(String name) { this.name = name; }
-    public String getName() { return this.name; }
-    public String getId() { return this.name(); }
+
+    CodeStandardTypes(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId() {
+        return this.name();
+    }
 }
