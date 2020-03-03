ef78fa0e4d6a3d273be6ea6e892fa1cf0770f35f
Pavlo Maks <pavlo@solidstategroup.com>
2020-03-03 15:52:05 +0000

    get codes admins only

--- diagnosisview-api/src/main/java/com/solidstategroup/diagnosisview/api/controller/AdminController.java
+++ diagnosisview-api/src/main/java/com/solidstategroup/diagnosisview/api/controller/AdminController.java
@@ -153,11 +153,13 @@ public class AdminController extends BaseController {
     @GetMapping("/codes")
     public List<CodeDto> getAllCodes(HttpServletRequest request) throws Exception {
 
-        User user = getUserFromRequest(request);
-        if (user != null &&
-                "University of Edinburgh".equalsIgnoreCase(user.getInstitution())) {
-            return codeService.getAll(Institution.UNIVERSITY_OF_EDINBURGH);
-        }
+//        User user = getUserFromRequest(request);
+//        if (user != null &&
+//                "University of Edinburgh".equalsIgnoreCase(user.getInstitution())) {
+//            return codeService.getAll(Institution.UNIVERSITY_OF_EDINBURGH);
+//        }
+
+        isAdminUser(request);
 
         return codeService.getAll(null);
     }
