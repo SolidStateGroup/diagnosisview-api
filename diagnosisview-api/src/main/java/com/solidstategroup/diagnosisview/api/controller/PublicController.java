0d94c9c87fd1aa1e165113dc68ba43f445efa04a
Pavlo Maks <pavlo@solidstategroup.com>
2020-04-15 10:33:55 +0100

    new public endpoint to return application settinsg

--- diagnosisview-api/src/main/java/com/solidstategroup/diagnosisview/api/controller/PublicController.java
+++ diagnosisview-api/src/main/java/com/solidstategroup/diagnosisview/api/controller/PublicController.java
@@ -1,10 +1,16 @@
 package com.solidstategroup.diagnosisview.api.controller;
 
+import com.solidstategroup.diagnosisview.model.codes.enums.DifficultyLevel;
+import com.solidstategroup.diagnosisview.model.codes.enums.Institution;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,5 +29,27 @@ public class PublicController {
         Map<String, String> response = new HashMap<>();
         response.put("status", "ok");
         return response;
+    }
+
+    /**
+     * Retrieves different configuration settings for the application.
+     *
+     * Used by FE.
+     *
+     * @return Map of configuration settings
+     */
+    @GetMapping(value = "/settings", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, List<?>> settings() {
+        return new HashMap<String, List<?>>() {{
+            put("institutions", Arrays.asList(Institution.UNIVERSITY_OF_EDINBURGH,
+                    Institution.NHS_SCOTLAND_KNOWLEDGE_NETWORK,
+                    Institution.UNIVERSITY_OF_MALAWI,
+                    Institution.OTHER,
+                    Institution.NONE));
+            put("difficultyLevels", Arrays.asList(DifficultyLevel.DO_NOT_OVERRIDE,
+                    DifficultyLevel.GREEN,
+                    DifficultyLevel.AMBER,
+                    DifficultyLevel.RED));
+        }};
     }
 }
