f7b89432cb1dd9487f9b8407581a57d6a0c3b3ef
Pavlo Maks <pavlo@solidstategroup.com>
2020-04-17 14:14:02 +0100

    code refactor

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/enums/InstitutionEnum.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/enums/InstitutionEnum.java
@@ -1,15 +1,15 @@
 package com.solidstategroup.diagnosisview.model.codes.enums;
 
-public enum Institution {
+public enum InstitutionEnum {
     UNIVERSITY_OF_EDINBURGH("University of Edinburgh"),
     NHS_SCOTLAND_KNOWLEDGE_NETWORK("NHS Scotland Knowledge Network"),
     UNIVERSITY_OF_MALAWI("University of Malawi"),
     OTHER("Other"),
-    NONE("NONE");
+    NONE("None");
 
     private String name;
 
-    Institution(String name) {
+    InstitutionEnum(String name) {
         this.name = name;
     }
 
