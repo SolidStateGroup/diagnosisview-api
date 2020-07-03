372d99136a085b4855627abccd989f6c13b0c449
Pavlo Maks <pavlo@solidstategroup.com>
2020-07-03 15:31:40 +0100

    implemented Tag wrapper

---
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/Tag.java
@@ -0,0 +1,17 @@
+package com.solidstategroup.diagnosisview.model;
+
+import com.solidstategroup.diagnosisview.model.codes.Lookup;
+import com.solidstategroup.diagnosisview.model.codes.LookupWrapper;
+
+/**
+ * <p>
+ * Tag model represents a Lookup of a type (LookupType) Tag.
+ * </p>
+ *
+ * Created by Pavlo Maksymchuk.
+ */
+public class Tag extends LookupWrapper {
+    public Tag(Lookup lookup) {
+        super(lookup);
+    }
+}
