3d17131fbb3a85df4ae200ac24a1caaa1b9e4fa2
Pavlo Maks <pavlo@solidstategroup.com>
2020-07-03 15:31:53 +0100

    implemented Tag dto

---
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/TagDto.java
@@ -0,0 +1,26 @@
+package com.solidstategroup.diagnosisview.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.solidstategroup.diagnosisview.model.codes.Lookup;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * POJO used to map Tag Lookup for FE.
+ */
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TagDto implements Serializable {
+    private String id;
+    private String name;
+
+    public TagDto(Lookup lookup){
+        this.id = lookup.getValue();
+        this.name = lookup.getDescription();
+    }
+}
+
+
