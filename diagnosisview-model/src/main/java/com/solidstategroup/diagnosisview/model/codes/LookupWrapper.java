6df2452c2a7d2b976e563f5a94b9c5dd30f5ef37
Pavlo Maks <pavlo@solidstategroup.com>
2020-07-06 13:30:07 +0100

    refactored LookupWrapper

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/LookupWrapper.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/LookupWrapper.java
@@ -1,26 +1,48 @@
 package com.solidstategroup.diagnosisview.model.codes;
 
+import java.io.Serializable;
+
 /**
  * Abstract class to wrap Lookup entity.
  * <p>
  * Created by Pavlo Maksymchuk.
  */
-public abstract class LookupWrapper {
-    protected Lookup lookup;
+public abstract class LookupWrapper implements Serializable {
+    private Long id;
+    private String code;
+    private String description;
+
+
+    public LookupWrapper() {
+    }
 
     public LookupWrapper(Lookup lookup) {
-        this.lookup = lookup;
+        this.id = lookup.getId();
+        this.code = lookup.getValue();
+        this.description = lookup.getDescription();
     }
 
     public Long getId() {
-        return lookup.getId();
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCode() {
-        return lookup.getValue();
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public String getDescription() {
-        return lookup.getDescription();
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
