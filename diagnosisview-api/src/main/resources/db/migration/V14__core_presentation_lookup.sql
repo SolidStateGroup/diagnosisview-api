5575a09a6812901663243c1993e6c1716e74dcdf
Pavlo Maks <pavlo@solidstategroup.com>
2020-07-10 14:25:34 +0100

    new tag look up type

---
+++ diagnosisview-api/src/main/resources/db/migration/V14__core_presentation_lookup.sql
@@ -0,0 +1,3 @@
+-- Core Presentation Tag Types
+INSERT INTO pv_lookup_value(id, creation_date, value, description, display_order, created_by, lookup_type_id)
+VALUES (146, now(), 'CORE_PRESENTATION','Core Presentation',1, null, 24);
