f14f3124eddad80d1ee0120eb088f2b2bac18447
Pavlo Maks <pavlo@solidstategroup.com>
2020-08-05 20:55:19 +0100

    refactored institution model

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/Institution.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/Institution.java
@@ -1,13 +1,49 @@
 package com.solidstategroup.diagnosisview.model.codes;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.solidstategroup.diagnosisview.model.codes.enums.LookupTypes;
+import io.swagger.models.auth.In;
+
+import java.util.Map;
+
 /**
  * Institution model represents a Lookup of a type (LookupType) Institution.
- *
- *
+ * <p>
  * Created by Pavlo Maksymchuk.
  */
-public class Institution extends LookupWrapper{
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Institution extends LookupWrapper {
+
+    private boolean hidden = false;
+    private Map<String, Object> stats;
+
+    public Institution(){
+        super();
+    }
+
     public Institution(Lookup lookup) {
         super(lookup);
+
+        // extra data is stored as map in jsonb field
+        // against lookup table
+        if (lookup.getData() != null) {
+            hidden = (boolean) lookup.getData().get("hidden");
+        }
+    }
+
+    public Map<String, Object> getStats() {
+        return stats;
+    }
+
+    public void setStats(Map<String, Object> stats) {
+        this.stats = stats;
+    }
+
+    public Boolean getHidden() {
+        return hidden;
+    }
+
+    public void setHidden(Boolean hidden) {
+        this.hidden = hidden;
     }
 }
