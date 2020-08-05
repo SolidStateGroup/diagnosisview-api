73ceff9f3bebceb46d8be9fee0095318bd4f7fd2
Pavlo Maks <pavlo@solidstategroup.com>
2020-08-05 20:40:58 +0100

    implemented get institution list

--- diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/impl/InstitutionService.java
+++ diagnosisview-service/src/main/java/com/solidstategroup/diagnosisview/service/impl/InstitutionService.java
@@ -1,11 +1,17 @@
 package com.solidstategroup.diagnosisview.service.impl;
 
+import com.google.common.collect.ImmutableMap;
 import com.solidstategroup.diagnosisview.exceptions.ResourceNotFoundException;
 import com.solidstategroup.diagnosisview.model.InstitutionDto;
 import com.solidstategroup.diagnosisview.model.codes.Institution;
+import com.solidstategroup.diagnosisview.model.codes.Lookup;
+import com.solidstategroup.diagnosisview.model.codes.LookupType;
 import com.solidstategroup.diagnosisview.model.codes.enums.LookupTypes;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +22,8 @@ import java.util.stream.Collectors;
  * <p>
  * Created by Pavlo Maksymchuk.
  */
+@Transactional(readOnly = true)
+@Slf4j
 @Service
 public class InstitutionService {
 
@@ -28,12 +36,29 @@ public class InstitutionService {
 
     /**
      * Find Institution by given lookup value
+     *
      * @param value
      * @return
      * @throws ResourceNotFoundException
      */
     public Institution getInstitution(String value) throws ResourceNotFoundException {
         return new Institution(lookupManager.findByTypeAndValue(LookupTypes.INSTITUTION_TYPE, value));
+    }
+
+    /**
+     * Find all Institutions in the system
+     *
+     * @return a List of Institutions
+     */
+    public List<Institution> getAll() {
+        return lookupManager.findByType(LookupTypes.INSTITUTION_TYPE)
+                .stream()
+                .map(lookup -> {
+                    Institution i = new Institution(lookup);
+                    i.setStats(lookupManager.getInstitutionStats(i.getCode()));
+                    return i;
+                })
+                .collect(Collectors.toList());
     }
 
     /**
