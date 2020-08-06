27502995383a60911c22fcad095466d22c644555
Pavlo Maks <pavlo@solidstategroup.com>
2020-08-06 16:37:35 +0100

    code refactor

--- diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/Institution.java
+++ diagnosisview-model/src/main/java/com/solidstategroup/diagnosisview/model/codes/Institution.java
@@ -1,6 +1,8 @@
 package com.solidstategroup.diagnosisview.model.codes;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Map;
 
@@ -11,7 +13,18 @@ import java.util.Map;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Institution extends LookupWrapper {
+    private static final String IMAGE_URL_TEMPLATE = "/api/institutions/%s/logo";
+    public static final String LOGO_DATA_FIELD = "logoData";
+    public static final String IMAGE_FORMAT_FIELD = "imageFormat";
+    public static final String HIDDEN_FIELD = "hidden";
 
+    // Used when FE sends content for logo
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String logoImage;
+
+    private String logoData;
+    private String imageFormat;
+    private String logoUrl;
     private boolean hidden = false;
     private Map<String, Object> stats;
 
@@ -24,9 +37,43 @@ public class Institution extends LookupWrapper {
 
         // extra data is stored as map in jsonb field
         // against lookup table
-        if (lookup.getData() != null) {
-            hidden = (boolean) lookup.getData().get("hidden");
+        if (!CollectionUtils.isEmpty(lookup.getData())) {
+            if (lookup.getData().containsKey(HIDDEN_FIELD)) {
+                hidden = (boolean) lookup.getData().get(HIDDEN_FIELD);
+            }
+            if (lookup.getData().containsKey(LOGO_DATA_FIELD)) {
+                logoData = (String) lookup.getData().get(LOGO_DATA_FIELD);
+                // if no image dont return url
+                logoUrl = String.format(IMAGE_URL_TEMPLATE, getId());
+            }
+            if (lookup.getData().containsKey(IMAGE_FORMAT_FIELD)) {
+                imageFormat = (String) lookup.getData().get(IMAGE_FORMAT_FIELD);
+            }
         }
+    }
+
+    public String getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(String logoImage) {
+        this.logoImage = logoImage;
+    }
+
+    public String getLogoData() {
+        return logoData;
+    }
+
+    public void setLogoData(String logoData) {
+        this.logoData = logoData;
+    }
+
+    public String getImageFormat() {
+        return imageFormat;
+    }
+
+    public void setImageFormat(String imageFormat) {
+        this.imageFormat = imageFormat;
     }
 
     public Map<String, Object> getStats() {
@@ -35,6 +82,14 @@ public class Institution extends LookupWrapper {
 
     public void setStats(Map<String, Object> stats) {
         this.stats = stats;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
     }
 
     public boolean isHidden() {
